2975fbcf46e8b01cd29bd8db2bb8bd303101a3b4
DaShaun <dashaun@redislabs.com>
2020-11-10 13:42:13 -0600

    first pass

---
+++ index.js
@@ -0,0 +1,75 @@
+const redis = require('redis');
+const uuid = require('uuid');
+
+let redisHost = process.env.REDIS_HOST;
+let redisPort = process.env.REDIS_PORT;
+let redisPass = process.env.REDIS_PASS;
+
+let redisClient = redis.createClient({host: redisHost, port: redisPort, password: redisPass});
+
+const thisMany = 200000;
+
+exports.consumer = (req, res) => {
+
+    redisClient.on('connect', function () {
+        console.log('Client Connected to ' + redisHost + ':' + redisPort);
+    })
+    redisClient.on("error", function (err) {
+        console.log("Error: " + err);
+    });
+
+    let currentKeys = [];
+
+    redisClient.keys('*', function (err, keys) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else {
+            console.log("Found " + keys.length + " keys.")
+            keys.forEach(function (key,i) {
+                currentKeys.push(key);
+            })
+        }
+    });
+
+    currentKeys.forEach(function(key){
+        redisClient.hgetall(key,function (err, engagement){
+            console.log(engagement);
+        })
+    })
+
+};
+
+exports.generator = (req, res) => {
+
+    redisClient.on('connect', function () {
+        console.log('Client Connected to ' + redisHost + ':' + redisPort);
+    });
+    redisClient.on("error", function (err) {
+        console.log("Error: " + err);
+    });
+
+    for (let i = 0; i <= thisMany; i++) {
+        console.log(`\tCreating hash ${i}`);
+
+        let deviceId = uuid.v4();
+        let branchId = uuid.v5();
+
+        redisClient.hmset(uuid.v1(), {
+            'in_out': 'in',
+            'deviceId': deviceId,
+            'branchId': branchId,
+            'timestamp': (new Date()).getTime()
+        });
+
+        redisClient.hmset(uuid.v1(), {
+            'in_out': 'out',
+            'deviceId': deviceId,
+            'branchId': branchId,
+            'timestamp': (new Date()).getTime() + Math.round(Math.random() * 900000) //some random time under 15 minutes later
+        });
+
+    }
+
+    res.status(200).send("Successfully created " + thisMany);
+};
