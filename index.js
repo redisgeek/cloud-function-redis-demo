c771506a7bc335d48a253100d48c57ccc54436b9
DaShaun <dashaun@redislabs.com>
2020-11-10 16:16:52 -0600

    add mysql create table and ingest

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const redis = require('redis');
 const uuid = require('uuid');
+const mysql = require('mysql');
 
 let redisHost = process.env.REDIS_HOST;
 let redisPort = process.env.REDIS_PORT;
 let redisPass = process.env.REDIS_PASS;
+
+let mysqlHost = process.env.MYSQL_HOST;
+let mysqlUser = process.env.MYSQL_USER;
+let mysqlPass = process.env.MYSQL_PASS;
+let mysqlDatabase = process.env.MYSQL_DB;
+
 let thisMany = process.env.THIS_MANY;
 
 let redisClient = redis.createClient({host: redisHost, port: redisPort, password: redisPass});
+
+let mysqlClient = mysql.createConnection({
+    host: mysqlHost,
+    user: mysqlUser,
+    password: mysqlPass,
+    database: mysqlDatabase
+});
+
+exports.createTable = (req, res) => {
+    mysqlClient.connect();
+
+    mysqlClient.query("CREATE TABLE ENGAGED( DeviceID varchar(255), BranchId varchar(255), in_out varchar(255), timestamp varchar(255) )");
+
+    res.status(200).send("Table Created");
+}
 
 exports.consumer = (req, res) => {
 
@@ -17,6 +39,8 @@ exports.consumer = (req, res) => {
         console.log("Error: " + err);
     });
 
+    mysqlClient.connect();
+
     redisClient.keys('*', function (err, keys) {
         if (err) {
             console.log(err);
@@ -26,6 +50,7 @@ exports.consumer = (req, res) => {
             keys.forEach(function (key,i) {
                 redisClient.hgetall(key,function (err, engagement){
                     console.log(engagement);
+                    mysqlClient.query('INSERT INTO ENGAGED (DeviceId, BranchId, in_out, timestamp) values (' + engagement.deviceId + ',' + engagement.branchId + ',' + engagement.in_out + ',' + engagement.timestamp + ')');
                     redisClient.del(key);
                 })
             })
